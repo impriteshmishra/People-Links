5f4c5b85cecddc065ec9edc8455eb162f2dd6739
Pritesh Mishra <priteshmishra2125@gmail.com>
2025-02-17 14:54:24 +0530

    fixes socket

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,7 +55,7 @@ function App() {
 
   useEffect(() => {
     if (user) {
-      const socketio = io("http://localhost:3500", {
+      const socketio = io("wss://people-links.onrender.com", {
         query: {
           userId: user?._id,
         },
